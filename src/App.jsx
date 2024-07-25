7f0862a19ac5bdbef171a68140fa271d38ebd2c5
Jad Sader <jadsaderkarate@gmail.com>
2024-07-25 18:39:10 +0300

    Finished register, added a JWT decode function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import ValidateRoute from "./components/utility/ValidateRoute";
 
@@ -17,6 +18,7 @@ function App() {
           }
         />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import LoginForm from "../components/forms/LoginForm";
-import { Typography } from "antd";
+import { Typography, Alert, Button } from "antd";
 const { Title } = Typography;
 
 function Login() {
+  const location = useLocation();
+  const [showSuccessMessage, setShowSuccessMessage] = useState(
+    !!location.state?.successMessage
+  );
+
+  useEffect(() => {
+    if (showSuccessMessage) {
+      const timer = setTimeout(() => {
+        setShowSuccessMessage(false);
+      }, 5000); // Hide the message after 5 seconds
+
+      return () => clearTimeout(timer); // Cleanup the timer on component unmount
+    }
+  }, [showSuccessMessage]);
+
   return (
     <div>
+      {showSuccessMessage && (
+        <Alert
+          message={location.state.successMessage}
+          type="success"
+          showIcon
+        />
+      )}
       <Title level={1} style={{ textAlign: "center" }}>
         Login
-      </Title>{" "}
+      </Title>
+
       <LoginForm />
     </div>
   );
