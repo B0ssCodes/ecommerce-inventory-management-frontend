3feee625c0832550fd6381656072789f4da1aaff
Jad Sader <jadsaderkarate@gmail.com>
2024-07-25 17:24:21 +0300

    Created Register Branch

---
+++ src/pages/Register.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography } from "antd";
+const { Title } = Typography;
+function Register() {
+  return (
+    <div>
+      <Title type={1} style={{ textAlign: "center" }}>
+        Register
+      </Title>
+    </div>
+  );
+}
+
+export default Register;
